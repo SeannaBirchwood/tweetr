7410a122f9d553fd7c0cb53d3966b1cc1c9a1ecc
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-10-21 17:42:35 +0000

    attempting to send form submit to db and back to page, no luck so far

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,16 +12,16 @@
 
 
  	//this takes json data and converts it to html friendly code
- 	var createTweetElement = (tweetData) => {
+ 	var createTweetElement = (loadInput) => {
  		var $tweet = `<article>
  			<header>
- 				<img class="profilePicture" src=${tweetData.user.avatars.small}>
- 				<h3> ${tweetData.user.name}</h3>
- 				<h4>${tweetData.user.handle}</h4>
+ 				<img class="profilePicture" src=${loadInput.user.avatars.small}>
+ 				<h3> ${loadInput.user.name}</h3>
+ 				<h4>${loadInput.user.handle}</h4>
  			</header>
- 				<p class="the-tweet">${tweetData.content.text}</p>
+ 				<p class="the-tweet">${loadInput.content.text}</p>
  			<footer>
- 				<p class="time-stamp">${tweetData.created_at}</p>
+ 				<p class="time-stamp">${loadInput.created_at}</p>
  				<span class="fa fa-flag" aria-hidden="true">
  			</span>
  				<span class="fa fa-retweet" aria-hidden="true">
@@ -32,85 +32,29 @@
  	} 
 
 
-
 	
 	//this loops through the json data and feeds it to the above function, then appends the result
-	function renderTweets(tweetData) {
-		tweetData.forEach(function (currentTweetObj) {
+	function renderTweets(loadInput) {
+		loadInput.forEach(function (currentTweetObj) {
 			$('.tweet-feed').append(createTweetElement(currentTweetObj));
-		});	
-	} 
+		});
+	}
 
+	var loadTweets = ($inputTweet) => {
+		var $inputTweet = $('form').on("submit"); 
+			//$.post("/", function (data) {
+        $('.tweet-feed').append();
+			$.ajax({
+				url: '/tweets',
+				dataType: "json",
+				success: function(response) {
+					renderTweets(response);
+				},
+				fail: ($inputTweet) => {
+				}
+			});
+	}
 
-	var $rendNewTweet = renderTweets(tweetData);
-
+	loadTweets();
 
 });
-
-/****************************************************************************************/
-
-/*****************************************Temp Database**********************************/ 
-
-var tweetData = [
- {
- 	"user": {
- 		"name": "Newton",
- 		"avatars": {
- 			"small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
- 			"regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
- 			"large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
- 		},
- 		"handle": "@SirIsaac"
- 	},
- 	"content": {
- 		"text": "If I have seen further it is by standing on the shoulders of giants"
- 	},
- 	"created_at": 1461116232227
- },
- {
- 	"user": {
- 		"name": "Descartes",
- 		"avatars": {
- 			"small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
- 			"regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
- 			"large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
- 		},
- 		"handle": "@rd" },
- 		"content": {
- 			"text": "Je pense , donc je suis"
- 		},
- 		"created_at": 1461113959088
- 	},
- 	{
- 		"user": {
- 			"name": "Johann von Goethe",
- 			"avatars": {
- 				"small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
- 				"regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
- 				"large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
- 			},
- 			"handle": "@johann49"
- 		},
- 		"content": {
- 			"text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
- 		},
- 		"created_at": 1461113796368
- 	}
- 	];
-
-
-// var tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": {
-//       "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//     },
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-// }
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,22 @@
 $(document).ready( function() { 
-	$("textarea").on("keyup", function() {
+
+	$("textarea").on("keyup", function(e) {
 		var max = 140;
   	var len = $(this).val().length;
   	var char = max - len;
-  	if (len >= max) {
-    $(".counter").text(char).css("color", "red");
-  } else {
-    $(".counter").text(char);
-  }
+  	if (len > max) {
+    	$(".counter").text(char).css("color", "red");
+    	$(".counter").text(char).parent().after("<div class='validation' style='color:black;margin-bottom: 20px;'>Hum Less!</div>");
+    	$( "form" ).on( "submit", function( event ) {
+        event.preventDefault();
+      });
+  	} else if (char === 140 || null || "") {
+  		$(".counter").parent().after("<div class='validation' style='color:black;margin-bottom: 20px;'>Hum something?</div>");
+    	$( "form" ).on( "submit", function( event ) {
+        event.preventDefault();
+      });
+  	} else {
+    	$(".counter").text(char).parent().next(".validation").remove()
+  	}
 	});
-	// var icon = $(".fa-flag") + $('.fa-retweet') + $('.fa-heart')
-	// $("article").on("hover", function() {
-	// 	$("footer").children("icon").append
-	// })
  });
