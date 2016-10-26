6640b285e1f4692567fa818c0e7e246f47d648f4
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-10-26 21:30:39 +0000

    Updated style, fixed counter. Haven't fixed form submission yet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,18 +7,10 @@ $(document).ready( function() {
   	var char = max - len;
 
   	if (len > max) {
-    	$(".counter").text(char).css("color", "red");
-    	$(".counter").text(char).parent().after("<div class='validation' style='color:black;margin-bottom: 20px;'>Hum Less!</div>");
-    	$( "form" ).on( "submit", function( event ) {
-        event.preventDefault();
-      });
-  	} else if (char === 140 || null || "") {
-  		$(".counter").parent().after("<div class='validation' style='color:black;margin-bottom: 20px;'>Hum something?</div>");
-    	$( "form" ).on( "submit", function( event ) {
-        event.preventDefault();
-      });
+      $(".counter").text(char).css("color", "red");
+    	//$(".counter").text(char).parent().after("<div class='validation' style='color:black;margin-bottom: 20px;'>Hum Less!</div>");
   	} else {
-    	$(".counter").text(char).parent().next(".validation").remove()
+    	$(".counter").text(char).css("color", "black"); //parent().next(".validation").remove()
   	}
 	});
 });
